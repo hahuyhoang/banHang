67f349bd4d384dbf4c9883ce6063a2ff8def81bb
hahuyhoang <huyhoang24062000@gmail.com>
2022-08-16 22:10:58 +0700

    Commit

---
+++ main.js
@@ -0,0 +1,53 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("cound-down").innerHTML = days + "d " +" : " + hours + "h " +" : "
+  + minutes + "m " +" : " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("cound-down").innerHTML = "EXPIRED";
+  }
+}, 1000);
+
+let slideIndex = 1;
+        showSlides(slideIndex);
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function showSlides(n) {
+        let i;
+        let slides = document.getElementsByClassName("prew");
+        if (n > slides.length) {slideIndex = 1}
+        if (n < 1) {slideIndex = slides.length}
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slides[slideIndex-1].style.display = "block"; 
+
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        setTimeout(showSlides, 2000); // Change image every 2 seconds
+    }
+
+
